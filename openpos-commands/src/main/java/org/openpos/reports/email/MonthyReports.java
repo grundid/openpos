0cc0bf206f1cea3a9c190250a7601a7e8f811cba
Adrian Stabiszewski <as@grundid.de>
2017-10-06 14:45:29 +0200

    report up to 12 months back if report is missing

--- openpos-commands/src/main/java/org/openpos/reports/email/MonthyReports.java
+++ openpos-commands/src/main/java/org/openpos/reports/email/MonthyReports.java
@@ -1,38 +1,49 @@
 package org.openpos.reports.email;
 
-import java.util.Calendar;
-
 import org.openpos.reports.IReportsPublishService;
 import org.openpos.timerecording.dao.MonthlyReportingInsert;
 import org.openpos.timerecording.dao.MonthlyReportingSelect;
 import org.openpos.utils.LegacyFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 @Service
 public class MonthyReports {
 
-	@Autowired
-	private LegacyFactory legacyFactory;
-	@Autowired
-	private IReportsPublishService reportsPublishService;
-	@Autowired
-	private MonthlyReportingSelect monthlyReportingSelect;
-	@Autowired
-	private MonthlyReportingInsert monthlyReportingInsert;
+    private Logger log = LoggerFactory.getLogger(getClass());
 
-	public void startReporting() {
-		Calendar lastMonth = Calendar.getInstance();
-		lastMonth.set(Calendar.DAY_OF_MONTH, 1);
-		lastMonth.add(Calendar.DAY_OF_MONTH, -1);
-		int month = lastMonth.get(Calendar.MONTH);
-		int year = lastMonth.get(Calendar.YEAR);
-		MonthlyReporting monthlyReporting = monthlyReportingSelect.findReporting(month, year);
-		if (monthlyReporting == null) {
-			MonthyReportsSender sender = new MonthyReportsSender(month, year, legacyFactory, reportsPublishService,
-					monthlyReportingInsert);
-			Thread thread = new Thread(sender);
-			thread.start();
-		}
-	}
+    @Autowired
+    private LegacyFactory legacyFactory;
+    @Autowired
+    private IReportsPublishService reportsPublishService;
+    @Autowired
+    private MonthlyReportingSelect monthlyReportingSelect;
+    @Autowired
+    private MonthlyReportingInsert monthlyReportingInsert;
+
+    public void startReporting() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM YYYY");
+        Calendar lastMonth = Calendar.getInstance();
+        lastMonth.set(Calendar.DAY_OF_MONTH, 1);
+        lastMonth.add(Calendar.DAY_OF_MONTH, -1);
+        lastMonth.set(Calendar.DAY_OF_MONTH, 1); // set first of month again
+        for (int x = 0; x < 12; x++) {
+            int month = lastMonth.get(Calendar.MONTH);
+            int year = lastMonth.get(Calendar.YEAR);
+            MonthlyReporting monthlyReporting = monthlyReportingSelect.findReporting(month, year);
+            if (monthlyReporting == null) {
+                log.info("Monatsreport für {}", dateFormat.format(lastMonth.getTime()));
+                MonthyReportsSender sender = new MonthyReportsSender(month, year, legacyFactory, reportsPublishService,
+                        monthlyReportingInsert);
+                Thread thread = new Thread(sender);
+                thread.start();
+            }
+            lastMonth.add(Calendar.MONTH, -1);
+        }
+    }
 }
